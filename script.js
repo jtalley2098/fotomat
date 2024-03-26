0d222ed7f6dfad31137ba1ec02c4cf70c13968c3
Jaa'Tal <jtalisman99@gmail.com>
2024-03-26 11:50:26 -0700

    scaled photos and mouseover popup

--- script.js
+++ script.js
@@ -21,11 +21,22 @@ joinLink.addEventListener("mouseout", () => {
 const colorChange = document.getElementById("butt");
 
 colorChange.addEventListener("mouseover", () => {
-  colorChange.style.backgroundColor = "#000000";
+  colorChange.style.backgroundColor = "#565656";
 });
 
 colorChange.addEventListener("mouseout", () => {
   colorChange.style.backgroundColor = "#4a4a4a";
+});
+
+const fourPics = document.querySelectorAll('.photos img');
+
+fourPics.forEach(pic => {
+  pic.addEventListener("mouseover", () => {
+    pic.style.transform = "scale(1.1)";
+  });
+  pic.addEventListener("mouseout", () => {
+    pic.style.transform = "scale(1)";
+  });
 });
 
 
