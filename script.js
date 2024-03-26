d54497fcb6658f23568a96c05fcc7454ed54d876
Jaa'Tal <jtalisman99@gmail.com>
2024-03-25 22:45:06 -0700

    files for cody

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-function ready(){
+
+
+
+
+
+function ready() {
+ 
+const joinLink = document.getElementById("join");
+const popUp = document.getElementById("popup");
+
+joinLink.addEventListener("mouseover", () => {
+  popUp.style.display = "block";
+});
+
+joinLink.addEventListener("mouseout", () => {
+  popUp.style.display = "none";
+});
+
+const colorChange = document.getElementById("butt");
+
+colorChange.addEventListener("mouseover", () => {
+  document.style.backgroundColor = "#565656";
+});
+
+colorChange.addEventListener("mouseout", () => {
+  document.style.backgroundColor = "#4a4a4a";
+});
 
     
-
-
-
-
 }
-document.addEventListener("DOMContentLoaded", ready);
+
+    document.addEventListener("DOMContentLoaded", ready);
+  
+  
+
